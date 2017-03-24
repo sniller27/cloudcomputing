637bbc67a026b81ef703b2686b1885ca8de03a74
Jonas <sniller27@hotmail.com>
2017-03-24 19:30:12 +0100

    user connect and disconnect messages

--- index.js
+++ index.js
@@ -15,17 +15,26 @@ app.get('/', function(req, res){
 //socket.io listens on the connection event for incoming sockets...executes when server receives request
 io.on('connection', function(socket){
 
-	console.log('a user connected');
+	//console.log('a user connected');
 
-	//"redirects" users input to console
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+	//"redirects" users input to console
+    //console.log('message: ' + msg);
+
+    //prints on website
+    io.emit('chat message', new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') + ' [John]: '  + msg);
+  });
+
+  socket.on('user connected', function(msg){
+    //prints on website
+    io.emit('chat message', '[John] has joined the chat.');
   });
 
   //prints message in console when user is disconnecting
-  // socket.on('disconnect', function(){
-  //   console.log('user disconnected');
-  // });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+    io.emit('chat message', '[John] has left the chat.');
+  });
 });
 
 //http server listen on port 3000
